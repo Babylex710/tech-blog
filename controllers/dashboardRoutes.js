1ef8520cadb24c1450a493d41c8908c1a3580438
Lexi M <104409081+Babylex710@users.noreply.github.com>
2022-12-20 07:17:30 -0800

    updated dashboardRoutes

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const { Post } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // Pass serialized data and session flag into template
+    res.render("userpost", {
+      layout: "dashboard",
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth to prevent access to route
+router.get("/newpost", withAuth, async (req, res) => {
+  res.render("newpost", {
+    layout: "dashboard",
+  });
+});
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    // Serialize data so the template can read it
+    const post = postData.get({ plain: true });
+    console.log("----post----");
+    console.log(post);
+    // Pass serialized data and session flag into template
+    res.render("update", {
+      layout: "dashboard",
+      post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
